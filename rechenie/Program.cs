de36116c8d634a2d14a92f6c07f38b109f566d71
Oksana <ksunia-nia@yandex.ru>
2022-06-08 18:25:31 +0300

    rechenie

---
+++ rechenie/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] array1 = new string[5] {"Официант, тарелка мокрая!", "Это", "ваш", "суп", "Cэр"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+     {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArray(array1, array2);
+PrintArray(array2);
